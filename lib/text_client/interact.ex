e6f2eab08c4a25e93d09acb4c8b1f0c296f6e502
Stephen Cefali <stephencefali@gmail.com>
2019-12-30 16:35:01 -0800

    initial

---
+++ lib/text_client/interact.ex
@@ -0,0 +1,21 @@
+defmodule TextClient.Interact do
+  alias TextClient.State
+
+
+  def start() do
+    Hangman.new_game()
+    |> setup_state()
+    |> IO.inspect()
+  end
+
+  defp setup_state(game) do
+    %State{
+      game_service: game,
+      tally: Hangman.tally(game),
+    }
+  end
+
+  def play(state) do
+    play(state)
+  end
+end
